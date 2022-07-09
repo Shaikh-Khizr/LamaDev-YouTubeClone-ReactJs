62f84ad5f7b2b44b4dc131494467f9b38823aff5
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-07-09 14:52:04 +0530

    Video Card Component Done.

---
+++ src/components/Card.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width: 360px;
+    margin-bottom: 45px;
+    cursor: pointer;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    height: 202px;
+    background-color: #999;
+`;
+
+const Details = styled.div`
+    display: flex;
+    margin-top: 16px;
+    gap: 12px;
+`;
+
+const ChannelImage = styled.img`
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    background-color: #999;
+`;
+
+const Texts = styled.div``;
+
+const Title = styled.h1`
+    font-size: 16px;
+    font-weight: 500;
+    color: ${({theme}) => theme.text};
+`;
+
+const ChannelName = styled.h2`
+    font-size: 14px;
+    color: ${({theme}) => theme.textSoft};
+    margin: 9px 0px;
+`;
+
+const Info = styled.div`
+    font-size: 14px;
+    color: ${({theme}) => theme.textSoft};
+`;
+
+const Card = () => {
+  return (
+    <Container>
+        <Image src="https://i.ytimg.com/vi/4mOkFXyxfsU/hqdefault.jpg" />
+        <Details>
+            <ChannelImage src="https://yt3.ggpht.com/wg1TITEoPfxvBGfzuqWyt3bqm_qu35ZhMswUv3feetU3xNX_6wsAXZF40OlPIgY4TmqbqCmAZ1U=s88-c-k-c0x00ffffff-no-rj" />
+            <Texts>
+                <Title>Build and Deploy a Modern Full Stack ECommerce React Application with Stripe</Title>
+                <ChannelName>JavaScript Mastery</ChannelName>
+                <Info>240,684 viewsApr 22, 2022</Info>
+            </Texts>
+        </Details>
+    </Container>
+  );
+};
+
+export default Card;
