2061323ef6b1f0695139e9ebedcf8fa97c76fc0d
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-07-10 12:32:38 +0530

    Implemented Props in Card Component.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,21 +3,25 @@ import styled from 'styled-components';
 import { Link } from "react-router-dom";
 
 const Container = styled.div`
-    width: 360px;
-    margin-bottom: 45px;
+    width: ${(props) => props.type !== "sm" && "360px"};
+    margin-bottom: ${(props) => props.type === "sm" ? "10px" : "45px"};
     cursor: pointer;
+    display: ${(props) => props.type === "sm" && "flex"};
+    gap: 10px;
 `;
 
 const Image = styled.img`
     width: 100%;
-    height: 202px;
+    height: ${(props) => props.type === "sm" ? "120px" : "202px"};
     background-color: #999;
+    flex: 1;
 `;
 
 const Details = styled.div`
     display: flex;
-    margin-top: 16px;
+    margin-top: ${(props) => props.type !== "sm" && "16px"};
     gap: 12px;
+    flex: 1;
 `;
 
 const ChannelImage = styled.img`
@@ -25,12 +29,13 @@ const ChannelImage = styled.img`
     height: 36px;
     border-radius: 50%;
     background-color: #999;
+    display: ${(props) => props.type === "sm" && "none"};
 `;
 
 const Texts = styled.div``;
 
 const Title = styled.h1`
-    font-size: 16px;
+    font-size: ${(props) => props.type === "sm" ? "13px" : "16px"};
     font-weight: 500;
     color: ${({theme}) => theme.text};
 `;
@@ -46,15 +51,15 @@ const Info = styled.div`
     color: ${({theme}) => theme.textSoft};
 `;
 
-const Card = () => {
+const Card = ({ type }) => {
   return (
     <Link to="/video/test" style={{ textDecoration: "none" }}>
-        <Container>
-            <Image src="https://i.ytimg.com/vi/4mOkFXyxfsU/hqdefault.jpg" />
-            <Details>
-                <ChannelImage src="https://yt3.ggpht.com/wg1TITEoPfxvBGfzuqWyt3bqm_qu35ZhMswUv3feetU3xNX_6wsAXZF40OlPIgY4TmqbqCmAZ1U=s88-c-k-c0x00ffffff-no-rj" />
+        <Container type={type}>
+            <Image type={type} src="https://i.ytimg.com/vi/4mOkFXyxfsU/hqdefault.jpg" />
+            <Details type={type}>
+                <ChannelImage type={type} src="https://yt3.ggpht.com/wg1TITEoPfxvBGfzuqWyt3bqm_qu35ZhMswUv3feetU3xNX_6wsAXZF40OlPIgY4TmqbqCmAZ1U=s88-c-k-c0x00ffffff-no-rj" />
                 <Texts>
-                    <Title>Build and Deploy a Modern Full Stack ECommerce React Application with Stripe</Title>
+                    <Title type={type}>Build and Deploy a Modern Full Stack ECommerce React Application with Stripe</Title>
                     <ChannelName>JavaScript Mastery</ChannelName>
                     <Info>242,000 views • Apr 22, 2022</Info>
                 </Texts>
